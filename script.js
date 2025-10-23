a7b4d51f1a59895e592e7c489f7a94e1834f3c26
TheRudraBro <rudramraj22@gmail.com>
2025-10-23 12:21:33 +0600

    Updated

--- script.js
+++ script.js
@@ -88,3 +88,15 @@ resetButton.addEventListener('click', function() {
 });
 
 updateDisplay();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 80,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
