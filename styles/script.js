18bad3849fc14d7739a2c315f5a72a7a89400ee2
TheRudraBro <rudramraj22@gmail.com>
2025-11-02 23:43:52 +0600

    Updated

--- styles/script.js
+++ styles/script.js
@@ -100,3 +100,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+
+
+
+// Mobile menu toggle
+  const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
