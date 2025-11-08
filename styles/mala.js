ef4ce8ce5ae379fc7219535777c53a127d2250cc
TheRudraBro <rudramraj22@gmail.com>
2025-11-08 20:48:01 +0600

    Updated

--- styles/mala.js
+++ styles/mala.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtn = document.getElementById("menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
-
   if (menuBtn && mobileMenu) {
     menuBtn.addEventListener("click", () => {
       mobileMenu.classList.toggle("hidden");
-
-
       menuBtn.textContent = mobileMenu.classList.contains("hidden") ? "☰" : "☰";
     });
   }
